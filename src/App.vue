e764b7b35b59a5d128888f22a14501b0504bdf5c
Megan Honig <megan@voicethread.net>
2026-06-10 16:03:53 -0400

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <HelloWorld
+    :some-prop="12345 as EpochTimeStamp"
+  />
+</template>
+
+
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+
+
+</script>
+
